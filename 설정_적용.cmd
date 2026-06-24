40e09239aeccb29e6ce1f3438242a22b2e3612f2
Rainy <238183989+rainy-me@users.noreply.github.com>
2026-06-25 01:27:41 +0900

    Change cmd

--- 설정_적용.cmd
+++ 설정_적용.cmd
@@ -1,3 +1,4 @@
 xcopy /e /i /y cfg "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\cfg"
+xcopy /e /i /y data "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\addons\sourcemod\data"
 copy /y "motd_rainy_en.txt" "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2"
 copy /y "motd_rainy_ko.txt" "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2"
