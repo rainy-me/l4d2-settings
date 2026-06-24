2ff9b285ab3f366b50c213f562a44e52930cc0d2
Rainy <238183989+rainy-me@users.noreply.github.com>
2026-06-24 20:41:10 +0900

    motd 추가

--- cfg 복사.cmd
+++ cfg 복사.cmd
@@ -1,1 +1,3 @@
 xcopy /e /i /y cfg "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\cfg"
+copy /y "motd_rainy_en.txt" "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2"
+copy /y "motd_rainy_ko.txt" "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2"
