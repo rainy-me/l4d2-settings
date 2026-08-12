4ee5afc4eacf54c571c28f47ce6978111b8b49e6
Rainy <238183989+rainy-me@users.noreply.github.com>
2026-08-12 21:46:42 +0900

    Update 설정_적용.cmd

--- 설정_적용.cmd
+++ 설정_적용.cmd
@@ -1,3 +1,4 @@
 xcopy /e /i /y cfg "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\cfg"
 xcopy /e /i /y data "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\addons\sourcemod\data"
+xcopy /e /i /y neko "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\neko"
 copy /y "motd.txt" "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2"
